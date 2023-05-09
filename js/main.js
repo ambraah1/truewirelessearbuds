1d67b910b88fce0715cbc0e5cebebfb0555fad27
ambraah1 <abrahamav011@gmail.com>
2023-05-09 09:42:46 -0400

    Añade cambio de color y borde al botón hamburguesa al alternar temas

--- js/main.js
+++ js/main.js
@@ -13,6 +13,20 @@ function updateThemeButton() {
     }
 }
 
+function changeNavbarTogglerIconColor() {
+    const navbarTogglerIcon = document.querySelector('.navbar-toggler-icon');
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const isDarkTheme = themeStylesheet.getAttribute('href') === 'css/dark-theme.css';
+
+    if (isDarkTheme) {
+        navbarTogglerIcon.style.filter = 'invert(0%)';
+        navbarToggler.style.borderColor = 'rgba(255, 255, 255, 0.1)';
+    } else {
+        navbarTogglerIcon.style.filter = 'invert(100%)';
+        navbarToggler.style.borderColor = 'rgba(0, 0, 0, 0.1)';
+    }
+}
+
 themeToggle.addEventListener('click', () => {
     if (themeStylesheet.getAttribute('href') === 'css/light-theme.css') {
         themeStylesheet.setAttribute('href', 'css/dark-theme.css');
@@ -20,7 +34,9 @@ themeToggle.addEventListener('click', () => {
         themeStylesheet.setAttribute('href', 'css/light-theme.css');
     }
     updateThemeButton();
+    changeNavbarTogglerIconColor();
 });
 
 // Inicializa el texto y el color del botón según el tema actual
 updateThemeButton();
+changeNavbarTogglerIconColor();
